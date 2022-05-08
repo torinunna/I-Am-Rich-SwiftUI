e7d858122cf07d55a78035d89da2b80825a5563a
torinunna <torinunna9588@gmail.com>
2022-05-08 23:53:49 +0900

    Initial Commit

---
+++ I Am Rich SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  I Am Rich SwiftUI
+//
+//  Created by 권유진 on 2022/05/08.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ I Am Rich SwiftUI/I_Am_Rich_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_Rich_SwiftUIApp.swift
+//  I Am Rich SwiftUI
+//
+//  Created by 권유진 on 2022/05/08.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_Rich_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
